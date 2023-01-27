664ec6db139416126623e4e81e2e47646a6b9b26
CantalupoAgustin <123779097+CantalupoAgustin@users.noreply.github.com>
2023-01-27 19:21:21 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+function enlace() {
+    window.open("https://www.argentina.gob.ar/economia/conocimiento/argentina-programa");
+  }
+
+  function link() {
+    window.open("https://www.foro3djuegos.com/foros/tema/41854536/0/listado-de-juegos-hechos-con-rpg-maker-que-merecen-la-pena/");
+  }
+
+  function link2() {
+    window.open("https://www.ticmas.com/");
+  }
